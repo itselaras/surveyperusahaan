cfe8780ae859eb135a2ff07dd52bd38034b9b425
LAPTOP-MI6RJP3A\ACER <ivanfabriano99@mail.ugm.ac.id>
2021-10-01 09:04:16 +0700

    UPDATE - Views Index Page Layout Bug

--- application/views/index.php
+++ application/views/index.php
@@ -1,137 +1,273 @@
 <!--
+
 Author: WebThemez
+
 Author URL: http://webthemez.com
+
 License: Creative Commons Attribution 3.0 Unported
+
 License URL: http://creativecommons.org/licenses/by/3.0/
+
 -->
+
 <!DOCTYPE html>
+
 <html lang="en">
+
 <head>
+
 	<meta charset="utf-8">
+
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<meta name="description" content="free-educational-responsive-web-template-webEdu">
+
 	<meta name="author" content="webThemez.com">
-	<title>Psikotes Sekolah</title>
+
+	<title>Survey Perusahaan</title>
+
 	<link rel="shortcut icon" href="<?php echo base_url('assets/images/favicon.ico')?>">
+
 	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.min.css')?>"> 
+
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap-theme.css')?>" media="screen"> 
+
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css')?>">
+
     <link rel='stylesheet' id='camera-css'  href="<?php echo base_url('assets/css/camera.css')?>" type='text/css' media='all'> 
+
 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+
 	<!--[if lt IE 9]>
+
 	<script src="assets/js/html5shiv.js"></script>
+
 	<script src="assets/js/respond.min.js"></script>
+
 	<![endif]-->
+
         <script type='text/javascript' src="<?php echo base_url('assets/js/jquery.min.js')?>"></script> 
 
+
+
 </head>
+
 <body>
+
 	<!-- Fixed navbar -->
+
 	<!--<div style="position: fixed; z-index: 100; right: 0px; left: 0px;" class="navbar navbar-inverse">-->
+
 	<!--	<div class="container">-->
+
 	<!--		<div class="navbar-header">-->
+
 				<!-- Button for smallest screens -->
+
 	<!--			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>-->
+
 	<!--			<a class="navbar-brand" href="<?php echo site_url('survey_controller/index')?>">-->
+
 	<!--				<img src="<?php echo base_url('assets/images/logo.png')?>" style="width: 100px; margin-top: -20px;" alt="Techro HTML5 template"></a>-->
+
 	<!--		</div>-->
+
 	<!--		<div class="navbar-collapse collapse">-->
+
 	<!--			<ul class="nav navbar-nav pull-right mainNav">-->
+
 	<!--				<li class="active"><a href="<?php echo site_url('survey_controller/index')?>">Beranda</a></li>-->
+
 	<!--				<li><a href="#about">Tentang</a></li>-->
+
 	<!--				<?php if ($this->session->logged_in){?>-->
+
 	<!--				<li><a href="<?php echo site_url('survey_controller/dashboard_survey')?>">Dashboard</a></li>-->
+
 	<!--				<li><a class="logbut" href="<?php echo site_url('survey_controller/logout')?>">Logout</a></li>-->
+
 	<!--				<?php } else {?>-->
+
 	<!--				<li><a class="logbut" href="<?php echo site_url('survey_controller/login')?>">Login</a></li>-->
+
  <!--                   <?php }?>-->
+
 	<!--			</ul>-->
+
 	<!--		</div>-->
+
 			<!--/.nav-collapse -->
+
 	<!--	</div>-->
+
 	<!--</div>-->
+
 	<!-- /.navbar -->
 
+
+
 	<!-- Header -->
+
 	<header id="head">
+
 		<div class="container">
+
              <div class="heading-text">							
+
 							<h1 class="animated flipInY delay1">Pelatihan dan Pengembangan</h1>
-							<p>Membantu Individu-individu untuk berkembang dengan optimal.</p>
+
+							<p class="mx-2 text-index" >Membantu Individu-individu untuk berkembang dengan optimal.</p>
+
 						</div>
+
             
+
 					<div class="fluid_container">                       
+
                     <div class="camera_wrap camera_emboss pattern_1" style="" id="camera_wrap_4">
+
                         <div data-thumb="<?php echo base_url('assets/images/slides/img1.jpg')?>" data-src="<?php echo base_url('assets/images/slides/img1.jpg')?>">
+
                             <h2>We develop.</h2>
+
                         </div> 
+
                         <div data-thumb="<?php echo base_url('assets/images/slides/img2.jpg')?>" data-src="<?php echo base_url('assets/images/slides/img2.jpg')?>">
+
                         </div>
+
                         <div data-thumb="<?php echo base_url('assets/images/slides/img3.jpg')?>" data-src="<?php echo base_url('assets/images/slides/img3.jpg')?>">
+
                         </div> 
+
                     </div><!-- #camera_wrap_3 -->
+
                 </div><!-- .fluid_container -->
+
 		</div>
+
 	</header>
+
 	<!-- /Header -->
 
+
+
   <div class="container" style="right: 50%;"  id="about">
+
     </div>
+
 	
+
       <section class="container" style="margin-top: 10px;">
-      <div class="row">
+
+      <div class="row px-3">
+
       	<div class="col-md-12"><div class="title-box clearfix "><h2 class="title-box_primary text-center">Layanan Bakat dan Minat Selaras</h2></div> 
+
         <p style="text-align: justify;text-justify: inter-word;">Setiap individu menyimpan potensi yang unik sejak lahir. Potensi tersebut berbentuk kecerdasan dan bakat. 
+
         Kecerdasan adalah kapasitas umum individu untuk bertindak secara terarah, berpikir secara rasional, dan menghadapi lingkungan secara efektif. 
+
         Sedangkan bakat adalah kapasitas seseorang untuk menguasai suatu hal atau aktivitas tertentu dengan sedikit sekali bergantung pada latihan. 
+
         Seiring proses perkembangannya, lingkungan ikut membentuk potensi tersebut. 
+
         Identifikasi dan optimalisasi potensi sejak dini menjadi sangat penting bagi individu guna mempersiapkan diri untuk meraih kesuksesan di masa yang akan datang.
+
         </br>
+
         </br>
+
         Dua hal lain yang juga menunjang kesuksesan individu di masa yang akan datang adalah <span id="dots"> ... </span><span id="more" style="display: none;">kepribadian dan minat. Kepribadian adalah kekhasan yang ada dalam diri individu yang membuatnya berbeda dengan individu yang lain, dimana hal tersebut dapat berubah. 
+
         Sedangkan minat berbeda dengan bakat. 
+
         Minat adalah kekuatan pendorong yang membuat individu memberikan perhatian lebih kepada suatu objek, dimana objek tersebut dapat berupa benda, aktivitas, ataupun orang.
+
         </br>
+
         </br>        
+
         Tentunya apabila kita mengetahui bagaimana potensi yang ada dalam diri individu (kecerdasan & bakat) dan faktor lain yang menjadi penunjang untuk keberhasilan individu (kepribadian & minat) maka akan lebih mudah untuk mengarahkan individu menuju keberhasilan di masa mendatang.
+
         </br>
+
         </br>
+
         Menyadari kebutuhan-kebutuhan tersebut, maka kami <b>PT. SELARAS MITRA INTEGRA</b> mengajak untuk memahami potensi diri, serta langkah optimalisasinya.
+
         </br>
+
         </br>
+
         Serangkaian layanan telah kami rancang sedemikian rupa :
+
         </br>
+
           </t>a. berdasarkan kebutuhan serta tujuan yang ingin diraih individu. </br>
+
           </t>b. menggunakan metode yang disesuaikan dengan usia perkembangan individu. </br>
+
           </t>c. dan difasilitasi oleh tim kami yang telah berpengalaman dan kompeten di bidang pengembangan individu. </br>
+
         </br>
+
         Kami, <b>PT Selaras Mitra Integra</b>, berharap dapat menjadi partner untuk bersama-sama mempersiapkan individu sedini mungkin untuk dapat meraih tujuan yang mereka inginkan.
+
         </span></p>
+
         <button onclick="myFunction()" class="btn btn-primary" id="myBtn">Read more</button>
+
  </div>
+
  
+
          <script>
+
         function myFunction() {
+
           var dots = document.getElementById("dots");
+
           var moreText = document.getElementById("more");
+
           var btnText = document.getElementById("myBtn");
+
           if (dots.style.display === "none") {
+
             dots.style.display = "inline";
+
             btnText.innerHTML = "Read more"; 
+
             moreText.style.display = "none";
+
             numList.style.display = "inline";            
+
           } else {
+
             dots.style.display = "none";
+
             btnText.innerHTML = "Read less"; 
+
             moreText.style.display = "inline";
+
             }
+
         }
+
         </script>
 
+
+
       </div>
+
       </section>
+
       
+
     	 
+
     
